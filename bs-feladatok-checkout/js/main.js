83f3452966355999fcce185dc0640c778b5de0a8
Judit Orosz <orosz.judit@gmail.com>
2021-12-06 11:32:37 +0100

    js question

---
+++ bs-feladatok-checkout/js/main.js
@@ -0,0 +1,16 @@
+const country = ['USA', 'Magyarország'];
+const state = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky',
+    'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri',
+    'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+    'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina',
+    'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+    'Wisconsin', 'Wyoming'];
+
+const fillCountry = (country) => {
+    const countryOptions = country.map(item => `<option>${item}</option>`);
+    return countryOptions;
+}
+
+let countryHTML = document.querySelector('#country');
+country.insertAdjucentHTML('beforeEnd', countryOptions);
